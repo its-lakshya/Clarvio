9cc1a7cdd77040bb13b91fe959f7c9ecb22e7c0a
Lakshya Kumar <kumarlakshya101@gmail.com>
2025-07-15 15:09:57 +0530

    feat: implement navbar

---
+++ components/NavItems.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { cn } from '@/lib/utils';
+import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+import React from 'react'
+
+const navItems = [
+  {label: "Home", href:"/"},
+  {label: "Companions", href:"/companions"},
+  {label: "My Journey", href:"/my-journey"},
+]
+const NavItems = () => {
+  const pathName = usePathname();
+  return (
+    <div className='flex items-center gap-4'>
+      {navItems.map(({label, href}) => (
+        <Link href={href} key={label} className={cn(pathName === href && 'text-primary font-semibold')}>{label}</Link>
+      ))}
+    </div>
+  )
+}
+
+export default NavItems
